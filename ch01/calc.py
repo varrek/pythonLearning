99afc78c1f1404e345d8dae947d8722017b762ed
Ihor Kozlov <i.kozlov@levi9.com>
2018-05-15 14:10:30 +0300

    Adding median

--- ch01/calc.py
+++ ch01/calc.py
@@ -13,12 +13,21 @@ while True:
 	except EOFError:
 		break
 if len(digits) > 0:
-	sumd = sum(digits)
-	countd = len(digits)
-	print(digits)
-	print("Count: " + str(countd))
-	print('Min: ' + str(min(digits)))
-	print('Max: ' + str(max(digits)))
-	print('Avr' + str(sum(digits) / len(digits)))
-	print('Sum' + str(sumd))
+    countd = len(digits)
+    for j in range(len(digits) - 1, 0, -1):
+        for  i in range(j):
+            if digits[i] > digits[i + 1]:
+                digits[i], digits[i + 1] = digits[i + 1], digits[i]
+    sumd = sum(digits)
+    index = int(len(digits) / 2)
+    median = digits[index]
+    if index and index * 2 == len(digits):
+        median = (median + digits[index - 1]) / 2
+    print(digits)
+    print("Count: " + str(countd))
+    print('Min: ' + str(min(digits)))
+    print('Max: ' + str(max(digits)))
+    print('Avr' + str(sum(digits) / len(digits)))
+    print('Sum' + str(sumd))
+    print('Median' + str(median))
 
